c5c4e61f7c312a89d999a8229ff94473eb4b8c6e
maxwell <2162003275@qq.com>
2026-07-12 13:00:24 +0800

    Initial Commit

---
+++ HelloDanci/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloDanci
+//
+//  Created by Maxwell on 2026/7/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloDanci/HelloDanciApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloDanciApp.swift
+//  HelloDanci
+//
+//  Created by Maxwell on 2026/7/12.
+//
+
+import SwiftUI
+
+@main
+struct HelloDanciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
