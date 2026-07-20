8b61b90402f69c7125a0b23d38f81112fd5c0233
maxwell <2162003275@qq.com>
2026-07-20 14:08:04 +0800

    Initial commit - Liquid Glass word card UI

--- HelloDanci/ContentView.swift
+++ HelloDanci/ContentView.swift
@@ -1,21 +1,43 @@
-//
-//  ContentView.swift
-//  HelloDanci
-//
-//  Created by Maxwell on 2026/7/12.
-//
 
 import SwiftUI
 
+struct NoFlashButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+    }
+}
+
 struct ContentView: View {
+    @State private var offset: CGFloat = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [
+                // .black
+                // .white
+                // .indigo.opacity(0.5), .mint.opacity(0.5)
+                 .black, .white
+                // .white, .black
+            ], startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea()
+
+            Button {
+
+            } label: {
+                Text("hello")
+                    .font(.system(size: 80))
+                    .bold()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .buttonStyle(NoFlashButtonStyle())
+            .glassEffect(.clear.interactive(), in: RoundedRectangle(cornerRadius: 57, style: .continuous))
+            .padding(20)
+            .padding(.vertical, 100)
+            .shadow(
+                color: .black.opacity(0.08),
+                radius: 10
+            )
         }
-        .padding()
     }
 }
 
